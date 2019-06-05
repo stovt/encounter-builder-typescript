19a820a3262cecaf36597782e96611aa39610bde
Sergey Tovt <st@norse.co>
2019-06-05 10:07:02 +0300

    add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import * as React from 'react';
 
-export default () => (
+const App: React.FC = () => (
   <>
     <h1>Welcome to React Parcel Micro App!</h1>
     <p>Hard to get more minimal than this React app.</p>
   </>
 );
+
+export default App;
